38cf7bf442547b732d955a80dbdaff58907866e7
Алексей Мокавчук <easyfrag4905@gmail.com>
2022-10-19 17:22:54 +0300

    added some fixes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,11 +6,7 @@ POSTS_CNT = 10
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших к меньшим)
     posts = Post.objects.select_related('author', 'group')[:POSTS_CNT]
-    # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
     }
